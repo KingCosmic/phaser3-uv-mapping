013397268e0b9642b4f42aafdb0969f49646b8a7
KingCosmic <KingCosmicDev@gmail.com>
2022-05-12 11:58:17 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # phaser3-uv-mapping
 Repo for the opensourced uv mapping based on https://www.youtube.com/watch?v=HsOKwUwL1bE
+
+This package only works in webgl mode.
+
+# Installation
+current required phaser version is 3.60.0-beta.4 as this package relies on the spritefx pipelines.
+
+## npm
+```
+npm install -S phaser3-uv-mapping phaser@3.60.0-beta.4
+```
+
+# Usage
+Just import the file and create the pipeline
+
+```js
+import UVPipeline from 'phaser3-uv-mapping'
+
+// .....
+create() {
+  // create a the uv pipeline
+  const pipeline = this.renderer.pipelines.add('UVPipeline', new UVPipeline(this.game, 'default-uv-texture-key'))
+
+  // on any sprite or image you have set it's pipeline to the UVPipeline
+  sprite.setPipeline(pipeline)
+
+  // the pipeline will use the lookup texture provided when it's created by default but to override this object by object give it a lookupTexture property
+  // for example.
+
+  const phaserTexture = this.game.textures.getFrame('other-uv-texture')
+
+  if (phaserTexture) {
+    sprite.lookupTexture = phaserTexture.glTexture;
+  }
+
+  // to change the default texture it's just this easy.
+  pipeline.changeDefaultUVTexture('new-texture-key')
+}
+// .....
---
+++ src/index.ts
@@ -0,0 +1,133 @@
+/**
+ * @author      Austyn Studdard <cosmic@bardsballad.com>
+ */
+ import Phaser from 'phaser'
+
+ const fragShader = `
+ #define SHADER_NAME COLOR_SWAP
+ 
+ precision highp int;
+ precision highp float;
+ 
+ uniform sampler2D uMainSampler;
+ uniform sampler2D uSecondarySampler;
+ uniform vec2 uResolution;
+ 
+ varying vec2 outTexCoord;
+ 
+ void main ()
+ {
+   vec4 imgColor = texture2D(uMainSampler,  outTexCoord);
+ 
+   vec2 pos = (imgColor.rg * 255.0) / 15.0;
+ 
+   gl_FragColor = texture2D(uSecondarySampler, pos);
+   gl_FragColor.a = imgColor.a;
+ }
+ `
+ 
+ class UVPipeline extends Phaser.Renderer.WebGL.Pipelines.SpriteFXPipeline {
+ 
+   texture:string
+   glTexture!:WebGLTexture
+   defaultTexture!:WebGLTexture
+ 
+   constructor(game:Phaser.Game, texture:string) {
+ 
+     super({
+       game,
+       fragShader
+     })
+ 
+     this.texture = texture
+     this.changeDefaultUVTexture(this.texture)
+   }
+ 
+   changeDefaultUVTexture(texture:string) {
+     const phaserTexture = this.game.textures.getFrame(texture)
+ 
+     if (phaserTexture) {
+       this.defaultTexture = phaserTexture.glTexture;
+     }
+   }
+ 
+   bindAndDraw(source: Phaser.Renderer.WebGL.RenderTarget) {
+     var gl = this.gl;
+     var renderer = this.renderer;
+ 
+     this.set1i('uMainSampler', 0);
+     this.set1i('uSecondarySampler', 1);
+ 
+     renderer.popFramebuffer(false, false, false);
+ 
+     if (!renderer.currentFramebuffer)
+     {
+       gl.viewport(0, 0, renderer.width, renderer.height);
+     }
+ 
+     gl.activeTexture(gl.TEXTURE0);
+     gl.bindTexture(gl.TEXTURE_2D, source.texture)
+ 
+     gl.activeTexture(gl.TEXTURE1)
+     gl.bindTexture(gl.TEXTURE_2D, this.glTexture)
+ 
+     // @ts-ignore
+     var matrix = this._tempMatrix1.loadIdentity();
+ 
+     // @ts-ignore
+     var x = this.targetBounds.x;
+     // @ts-ignore
+     var y = this.targetBounds.y;
+ 
+     var xw = x + source.width;
+     var yh = y + source.height;
+ 
+     var x0 = matrix.getX(x, y);
+     var x1 = matrix.getX(x, yh);
+     var x2 = matrix.getX(xw, yh);
+     var x3 = matrix.getX(xw, y);
+ 
+     //  Regular verts
+     var y0 = matrix.getY(x, y);
+     var y1 = matrix.getY(x, yh);
+     var y2 = matrix.getY(xw, yh);
+     var y3 = matrix.getY(xw, y);
+ 
+     //  Flip verts:
+     // var y0 = matrix.getY(x, yh);
+     // var y1 = matrix.getY(x, y);
+     // var y2 = matrix.getY(xw, y);
+     // var y3 = matrix.getY(xw, yh);
+ 
+     this.batchVert(x0, y0, 0, 0, 0, 0, 0xffffff);
+     this.batchVert(x1, y1, 0, 1, 0, 0, 0xffffff);
+     this.batchVert(x2, y2, 1, 1, 0, 0, 0xffffff);
+     this.batchVert(x0, y0, 0, 0, 0, 0, 0xffffff);
+     this.batchVert(x2, y2, 1, 1, 0, 0, 0xffffff);
+     this.batchVert(x3, y3, 1, 0, 0, 0, 0xffffff);
+ 
+     this.flush();
+ 
+     renderer.resetTextures();
+ 
+     // No hanging references
+     // @ts-ignore
+     this.tempSprite = null;
+   }
+ 
+   onPreRender() {}
+ 
+   onDraw(target: Phaser.Renderer.WebGL.RenderTarget) {
+     this.drawToGame(target)
+   }
+ 
+   onDrawSprite(gameObject: Phaser.GameObjects.Sprite, target: Phaser.Renderer.WebGL.RenderTarget): void {
+     // @ts-ignore
+     if (!gameObject.lookupTexture) return this.glTexture = this.defaultTexture
+ 
+     // @ts-ignore
+     this.glTexture = gameObject.lookupTexture
+   }
+ }
+ 
+ export default UVPipeline
